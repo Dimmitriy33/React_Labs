37b89b0e26b10a4ab4d52b0af9282be80284bcad
Dmitriy <schalunov002@gmail.com>
2021-07-26 22:21:40 +0300

    init

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import "./styles/main.css";
+import "./styles/main.scss";
+// watch: native intellisense and file-peek for aliases from jsconfig.json and with none-js files doesn't work: https://github.com/microsoft/TypeScript/issues/29334
+import { StrictMode } from "react";
+import ReactDom from "react-dom";
+
+const AppContainer = () => (
+  <StrictMode>
+    <div>Helo world!</div>
+  </StrictMode>
+);
+
+ReactDom.render(<AppContainer />, document.getElementById("app"));
