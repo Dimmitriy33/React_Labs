e65b7546750a1206dc430c6b2b4e4d5a05f3dfe2
Dmitriy <schalunov002@gmail.com>
2021-08-18 12:05:47 +0300

    remove some comments and fix inaccuracies

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable import/no-extraneous-dependencies */
 const { merge } = require("webpack-merge");
-// const webpack = require("webpack");
 
 const common = require("./webpack.common");
 
@@ -10,16 +9,8 @@ module.exports = (env, argv) => {
 
   /** @type {import('webpack').Configuration} */
   const extendedConfig = {
-    // plugins: [
-    //   new webpack.DefinePlugin({
-    //     // it adds custom Global definition to the project like BASE_URL for index.html
-    //     "process.env": {
-    //       ADMIN: "Admin123",
-    //     },
-    //   }),
-    // ],
     mode: "development",
-    devtool: "eval-cheap-module-source-map", // option controls how source maps are generated (affects on build speed dramatically): https://v4.webpack.js.org/configuration/devtool/
+    devtool: "eval-cheap-module-source-map",
   };
 
   return merge(commonConfig, extendedConfig);
